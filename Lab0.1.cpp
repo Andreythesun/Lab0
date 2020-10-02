ff1e46009d7aa77a5a3554816aa4069caf5bf2bc
Andreythesun <tyshkivskiy5@gmail.com>
2020-10-02 15:38:49 +0300

    first commit

---
+++ Lab0.1.cpp
@@ -0,0 +1,19 @@
+﻿ #include <stdio.h>
+
+int
+main()
+{
+	int a;
+	int b;
+	printf("Input a divisible:");
+	scanf("%d", &a);
+	printf("Input a divisor:");
+	scanf("%d", &b);
+	if (b == 0)
+	{
+		printf("Can't divide by 0");
+		return 1;
+	};
+	printf("Remainder of division %d by %d is %d \n", a, b, (a % b));
+	return 0;
+}
